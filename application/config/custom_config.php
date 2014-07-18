20bea5fd88ad23d6f9b1b2e4fdb6d0503fe6cfbe
Monisha <mviswa01@seattlecentral.edu>
2014-07-17 18:51:38 -0700

    adding new bootswatch theme

---
+++ application/config/custom_config.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//custom_config.php - a place to save glogal settings
+
+$config['style']='flatly.css';
+$config['banner']='Default Banner';
+$config['title']='Default Title';
+$config['theme']='';
+$config['copyright']='Default Copyright';
+
+
+
